245bc346c69013dab81ec6950edf7eb8bec920c6
Rushitkumar Pragajibhai Jasoliya <rs673845@dal.ca>
2021-07-01 21:36:49 +0530

    Added Database Connection classes

---
+++ src/main/java/DatabaseLayer/DatabaseConnection/DatabaseConnection.java
@@ -0,0 +1,76 @@
+package DatabaseLayer.DatabaseConnection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DatabaseConnection implements IDatabaseConnection {
+
+    private static final String CONFIG_FILENAME = "config.properties";
+    private static final String DRIVER_CLASSNAME = "db.driverClassName";
+    private static final String DB_URL = "db.url";
+    private static final String DB_USERNAME = "db.username";
+    private static final String DB_PASSWORD = "db.password";
+
+    private static DatabaseConnection databaseConnection;
+    private static Connection connection;
+    private static String url;
+    private static String dbUsername;
+    private static String dbPassword;
+
+    private DatabaseConnection() {
+        InputStream inputStream = DatabaseConnection.class.getClassLoader().getResourceAsStream(CONFIG_FILENAME);
+        Properties prop = new Properties();
+        try {
+            if (inputStream != null) {
+                prop.load(inputStream);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String driverClassname = prop.getProperty(DRIVER_CLASSNAME);
+        url = prop.getProperty(DB_URL);
+        dbUsername = prop.getProperty(DB_USERNAME);
+        dbPassword = prop.getProperty(DB_PASSWORD);
+
+        try {
+            Class.forName(driverClassname);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (Exception exception) {
+            exception.getMessage();
+        }
+    }
+
+    public static DatabaseConnection createInstance() {
+        if (databaseConnection == null) {
+            databaseConnection = new DatabaseConnection();
+        }
+        return databaseConnection;
+    }
+
+    public Connection openDBConnection() {
+        try {
+            connection = DriverManager.getConnection(url, dbUsername, dbPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void closeDBConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            } else {
+                return;
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+    }
+}
