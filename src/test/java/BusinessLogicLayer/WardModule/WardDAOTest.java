3d7fda2d9324d061f34c00dd0a9c1a28a1aa28e4
Rushitkumar Pragajibhai Jasoliya <rs673845@dal.ca>
2021-07-08 10:10:57 +0530

    Dummy commit to check runner

--- src/test/java/BusinessLogicLayer/WardModule/WardDAOTest.java
+++ src/test/java/BusinessLogicLayer/WardModule/WardDAOTest.java
@@ -20,10 +20,9 @@ public class WardDAOTest {
   @Test
   void getBedsTest() {
     Ward ward = new Ward(1, "WARD A", "COVID", "C Wing", 100, "General");
-    Bed bed1 = new Bed(1, "10000-17", ward, false, "ICU");
+    Bed bed1 = new Bed(1, "10000-17", ward, true, "ICU");
     Bed bed2 = new Bed(1, "10002-12", ward, true, "ICU");
-    Bed bed3 = new Bed(1, "10002-1", ward, false, "General");
-    ArrayList<Bed> bedArrayList = new ArrayList<>(Arrays.asList(bed1, bed2, bed3));
+    ArrayList<Bed> bedArrayList = new ArrayList<>(Arrays.asList(bed1, bed2));
 
     WardDAO wardDAO = new WardDAO();
     assertEquals(bedArrayList.size(), wardDAO.getWardBeds(1).size(), "No. of Bed in ward is not matching");
@@ -35,7 +34,6 @@ public class WardDAOTest {
 
     Bed bed1 = new Bed(1, "10000-17", ward, false, "ICU");
     Bed bed2 = new Bed(1, "10002-12", ward, true, "ICU");
-    Bed bed3 = new Bed(1, "10002-1", ward, false, "General");
 
     PatientBed pb1 = new PatientBed("John Due", bed2, "patient1");
     PatientBed pb2 = new PatientBed("John Due", bed1, "patient2");
