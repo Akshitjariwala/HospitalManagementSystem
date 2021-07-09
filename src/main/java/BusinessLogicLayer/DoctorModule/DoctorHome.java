4121d8ddb2e090e08145d6907f93ef4b3c8ef0d2
Vishal Rakesh Jaiswal <vs928999@dal.ca>
2021-07-09 16:28:06 +0530

    Completed feature View appointments-- Created ViewAppointmentsDao test class and then built the class usind TDD

--- src/main/java/BusinessLogicLayer/DoctorModule/DoctorHome.java
+++ src/main/java/BusinessLogicLayer/DoctorModule/DoctorHome.java
@@ -1,5 +1,13 @@
 package BusinessLogicLayer.DoctorModule;
 
 public class DoctorHome {
+	
+	public void doctorHomePage()
+	{
+		do
+		{
+			
+		}while(true);
+	}
 
 }
--- src/main/java/BusinessLogicLayer/DoctorModule/ViewAppointmentsDao.java
+++ src/main/java/BusinessLogicLayer/DoctorModule/ViewAppointmentsDao.java
@@ -1,14 +1,53 @@
 package BusinessLogicLayer.DoctorModule;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import BusinessLogicLayer.AdminModule.Appointment;
+import DatabaseLayer.DatabaseConnection.DatabaseConnectionFactory;
+import DatabaseLayer.DatabaseConnection.IDatabaseConnection;
+import DatabaseLayer.DatabaseConnection.IDatabaseConnectionFactory;
 
 public class ViewAppointmentsDao {
+	Connection connection = null;
+	IDatabaseConnection databaseConnection;
+	IDatabaseConnectionFactory databaseConnectionFactory;
+
+	public ViewAppointmentsDao() {
+		databaseConnectionFactory = new DatabaseConnectionFactory();
+		databaseConnection = databaseConnectionFactory.getDatabaseConnection();
+	}
 
 	public ArrayList<Appointment> getAppointmentList(String query) {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		ArrayList<Appointment> appointments = new ArrayList<>();
+		connection = databaseConnection.openDBConnection();
+		PreparedStatement statement;
+		try {
+			statement = connection.prepareStatement(query);
 
+			ResultSet rs = statement.executeQuery();
+
+			while (rs.next()) {
+				Appointment appointment = new Appointment();
+				appointment.setAppointmentId(rs.getInt("app_id"));
+				appointment.setPatientId(rs.getString("patient_id"));
+				appointment.setDate(rs.getString("date"));
+				appointment.setPreferredSlot(rs.getString("preferred_slot"));
+				appointment.setTypeOfAppointment(rs.getString("type_of_appo"));
+				appointments.add(appointment);
+			}
+			return appointments;
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		} finally {
+			databaseConnection.closeDBConnection();
+		}
+		return new ArrayList<>();
+	}
 }
+
+
