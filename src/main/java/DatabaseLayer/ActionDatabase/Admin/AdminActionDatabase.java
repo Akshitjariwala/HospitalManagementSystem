770275a9b5184d401b9f9b3381d2aa0be1db742c
Rushitkumar Pragajibhai Jasoliya <rs673845@dal.ca>
2021-07-09 12:08:12 +0530

    Interface created for AdminActions and ManageWard database

---
+++ src/main/java/DatabaseLayer/ActionDatabase/Admin/AdminActionDatabase.java
@@ -0,0 +1,14 @@
+package DatabaseLayer.ActionDatabase.Admin;
+
+public class AdminActionDatabase implements IAdminActionDatabase {
+
+  @Override
+  public IManageWardDatabase manageWard() {
+    return null;
+  }
+
+  @Override
+  public IAdmissionDischargeDatabase admissionDischarge() {
+    return null;
+  }
+}
---
+++ src/main/java/DatabaseLayer/ActionDatabase/Admin/IAdminActionDatabase.java
@@ -0,0 +1,8 @@
+package DatabaseLayer.ActionDatabase.Admin;
+
+public interface IAdminActionDatabase {
+
+  IManageWardDatabase manageWard();
+
+  IAdmissionDischargeDatabase admissionDischarge();
+}
