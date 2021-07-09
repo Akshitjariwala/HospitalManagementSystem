ff57da0c14f8300d00289fe70b9ebf14679241e4
Vishal Rakesh Jaiswal <vs928999@dal.ca>
2021-07-09 16:37:55 +0530

    Solved pipeline failure

--- src/test/java/BusinessLogicLayer/DoctorModule/ViewAppointmentsTest.java
+++ src/test/java/BusinessLogicLayer/DoctorModule/ViewAppointmentsTest.java
@@ -3,40 +3,42 @@ package BusinessLogicLayer.DoctorModule;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import BusinessLogicLayer.AdminModule.Appointment;
 
 
 
 class ViewAppointmentsTest {
-	private ViewAppointmentsDao viewappointmentDAO;
+	private ViewAppointments viewappointment;
 
 	@BeforeEach
 	void setup() {
-		viewappointmentDAO = Mockito.mock(ViewAppointmentsDao.class);
+		viewappointment = Mockito.mock(ViewAppointments.class);
 	}
 
 	@Test
 	void viewappointments_confirmed() {
 		ViewAppointments v1=new ViewAppointments("janki123","CONFIRM");
+	    Mockito.when(viewappointment.viewAllAppointments()).thenReturn(true);
+
 		assertEquals(true,v1.viewAllAppointments());
 		
 	}
 	@Test
 	void viewappointments_pending() {
 		ViewAppointments v1=new ViewAppointments("janki123","Pending");
+	    Mockito.when(viewappointment.viewAllAppointments()).thenReturn(true);
 		assertEquals(true, v1.viewAllAppointments());
 	}
 	@Test
 	void viewappointments_NoAppointmentfound() {
 		ViewAppointments v1=new ViewAppointments("janki1234","CONFIRM");
+	    Mockito.when(viewappointment.viewAllAppointments()).thenReturn(false);
+
 		assertEquals(false,v1.viewAllAppointments());
 	}
 
