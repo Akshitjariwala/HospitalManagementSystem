d3d85a6f492d360866ba2fa171f5c5a6a46c5729
Kishan Kahodariya <ks805556@dal.ca>
2021-07-10 10:45:30 +0530

    input method changed from Scanner to BufferedReader

--- src/main/java/BusinessLogicLayer/PatientModule/PatientLogin.java
+++ src/main/java/BusinessLogicLayer/PatientModule/PatientLogin.java
@@ -2,6 +2,9 @@ package BusinessLogicLayer.PatientModule;
 
 import DatabaseLayer.DatabaseConnection.DatabaseConnection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -15,6 +18,7 @@ public class PatientLogin {
     private  static Connection connection=databaseConnection.openDBConnection();
     private Statement statement=null;
     private Scanner readInput=new Scanner(System.in);
+    private BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     public void patientLogin(){
         String userID=null;
@@ -25,27 +29,30 @@ public class PatientLogin {
         System.out.println("===========================================\n" +
                 "\t\t\t\tPatient Login\n" +
                 "===========================================\n");
-        do{
-            if(userID!=null || password!=null)
-                System.out.println("*** Invalid user ID or Password ***");
+       try {
+           do {
+               if (userID != null || password != null)
+                   System.out.println("*** Invalid user ID or Password ***");
 
-            System.out.println("User ID:");
-            userID=readInput.nextLine();
-            System.out.println("Password:");
-            password=readInput.nextLine();
+               System.out.println("User ID:");
+               userID =reader.readLine();
+               System.out.println("Password:");
+               password = reader.readLine();
 
-            try{
-                statement=connection.createStatement();
-                resultSet=statement.executeQuery("SELECT password FROM login_cred WHERE userid='"+userID+"';");
-                while (resultSet.next()){
-                    providedPassword=resultSet.getString(1);
-                }
-            }
-            catch (Exception E){
-                E.printStackTrace();
-            }
-        }while (!password.equals(providedPassword));
-        System.out.println("Welcome "+userID);
+               try {
+                   statement = connection.createStatement();
+                   resultSet = statement.executeQuery("SELECT password FROM login_cred WHERE userid='" + userID + "';");
+                   while (resultSet.next()) {
+                       providedPassword = resultSet.getString(1);
+                   }
+               } catch (Exception E) {
+                   E.printStackTrace();
+               }
+           } while (!password.equals(providedPassword));
+           System.out.println("Welcome " + userID);
+       }catch (IOException e){
+           System.out.println("I/O ERROR");
+       }
     }
 
     public static void main(String[] args) {
