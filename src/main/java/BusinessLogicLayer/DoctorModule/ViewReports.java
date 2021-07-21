1cdc0ee028a125cec92a660bd913573c71ee6152
Vishal Rakesh Jaiswal <vs928999@dal.ca>
2021-07-21 12:21:26 +0200

    Developed viewallreports() in doctor module

--- src/main/java/BusinessLogicLayer/DoctorModule/ViewReports.java
+++ src/main/java/BusinessLogicLayer/DoctorModule/ViewReports.java
@@ -1,22 +1,52 @@
 package BusinessLogicLayer.DoctorModule;
 
+import java.util.ArrayList;
+
+import BusinessLogicLayer.BeanClasses.Reports;
+import DatabaseLayer.Dao.ViewReportsDao;
+
 public class ViewReports {
 
 	String docId;
+	ViewReportsDao vrdao;
 	public ViewReports(String docId) {
 		// TODO Auto-generated constructor stub
 		this.docId=docId;
-		
+		vrdao=new ViewReportsDao();		
 	}
-
 	public boolean viewAllReports() {
 		// TODO Auto-generated method stub
-		return false;
-	}
+		ArrayList<Reports> reports=new ArrayList<Reports>();
+		reports=vrdao.getAllReports(this.docId);
+		if(reports==null)
+			return false;
+		if(reports!=null && reports.size()>0) {
+			System.out.println("================================================================");
+			System.out.println("\t\t\t\tAll Reports under your supervision\t\t\t\t");
+			System.out.println("================================================================");
+			System.out.printf("%n");
+			System.out.printf("%-10s %-10s %-20s", "Report Id", " | ", "PatientID");
+			System.out.printf("%-20s %-10s %-20s", "Diagnosis Name", " | ", "Report Date");
+			System.out.printf("%-50s %-10s %-30s", "Result", " | ", "Report Type");
+			for(Reports report:reports)
+			{
+				System.out.printf("%-10s %-10s %-20s", report.getReportId(), " | ", report.getPatientId());
+				System.out.printf("%-20s %-10s %-20s", report.getDiagnosisName(), " | ", report.getDate());
+				System.out.printf("%-50s %-10s %-30s", report.getTestResult(), " | ",report.getTestType());
+			}
+			return true;
+		}
+		else {
+			System.out.println("==================================================");
+			System.out.println("\t\tCurrently no reports for patients under your supervision\t\t");
+			System.out.println("==================================================");
+			return false;
+		}
+}
 
-	public boolean viewPatientReports() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+public boolean viewPatientReports() {
+	// TODO Auto-generated method stub
+	return false;
+}
 
 }
--- src/main/java/DatabaseLayer/Dao/ViewReportsDao.java
+++ src/main/java/DatabaseLayer/Dao/ViewReportsDao.java
@@ -1,5 +1,17 @@
 package DatabaseLayer.Dao;
 
+import java.util.ArrayList;
+
+import BusinessLogicLayer.BeanClasses.Reports;
+import BusinessLogicLayer.PatientModule.Patient;
+
 public class ViewReportsDao {
+
+	
+
+	public ArrayList<Reports> getAllReports(String docId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
