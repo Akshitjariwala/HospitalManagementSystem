66228f2a517b4e8805cbd61dc347bc23a1eed13e
Vishal Rakesh Jaiswal <vs928999@dal.ca>
2021-07-21 16:13:55 +0200

    Completed Check Availability feature alongwith unit testing

--- src/test/java/DatabaseLayer/Dao/ViewReportsDaoTest.java
+++ src/test/java/DatabaseLayer/Dao/ViewReportsDaoTest.java
@@ -35,5 +35,21 @@ class ViewReportsDaoTest {
 
 		
 	}
+	@Test
+	void test_getAllPatientReports() {
+		Reports r1=new Reports();
+		r1.setReportId(1);
+		r1.setDoctorId(2);
+		r1.setDate("11-07-2021");
+		r1.setDiagnosisName("Covid-19");
+		r1.setPatientId("vishal123");
+		r1.setTestResult("Negative");
+		r1.setTestType("RTPCR");
+		ArrayList<Reports> report = new ArrayList<>(Arrays.asList(r1));
+	    Mockito.when(viewreportsDAO.getAllReports("janki123")).thenReturn(report);
+	    assertEquals(1, report.size());
+
+		
+	}
 
 }
