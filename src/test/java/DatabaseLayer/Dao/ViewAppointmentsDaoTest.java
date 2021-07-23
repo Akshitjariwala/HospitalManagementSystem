300c721621fcc6960a57c99705698007bc8e6039
Rushitkumar Pragajibhai Jasoliya <rs673845@dal.ca>
2021-07-23 11:24:35 -0300

    Failed test cases fixed

--- src/test/java/DatabaseLayer/Dao/ViewAppointmentsDaoTest.java
+++ src/test/java/DatabaseLayer/Dao/ViewAppointmentsDaoTest.java
@@ -13,21 +13,21 @@ import BusinessLogicLayer.BeanClasses.Appointment;
 
 class ViewAppointmentsDaoTest {
 
-	private ViewAppointmentsDao viewappointmentDAO;
+  private ViewAppointmentsDao viewappointmentDAO;
 
-	  @BeforeEach
-	  void setup() {
-	    viewappointmentDAO = Mockito.mock(ViewAppointmentsDao.class);
-	  }
+  @BeforeEach
+  void setup() {
+    viewappointmentDAO = Mockito.mock(ViewAppointmentsDao.class);
+  }
 
-	  @Test
-	  void getAppointmentListTest() {
-	    Appointment appointment1 = new Appointment(1, "joe999", "janki123", "09/07/2021", "Morning", "In Person", "Pending");
-	    Appointment appointment2 = new Appointment(2, "joe999", "janki123", "10/07/2021", "Morning", "In Person", "Pending");
-	    ArrayList<Appointment> appointments = new ArrayList<>(Arrays.asList(appointment1, appointment2));
-	    Mockito.when(viewappointmentDAO.getAppointmentList(1,"CONFIRMED")).thenReturn(appointments);
-	    assertEquals(2, appointments.size());
-	  }
+  @Test
+  void getAppointmentListTest() {
+    Appointment appointment1 = new Appointment(1, "joe999", "janki123", "09/07/2021", "Morning", "In Person", "Pending");
+    Appointment appointment2 = new Appointment(2, "joe999", "janki123", "10/07/2021", "Morning", "In Person", "Pending");
+    ArrayList<Appointment> appointments = new ArrayList<>(Arrays.asList(appointment1, appointment2));
+    Mockito.when(viewappointmentDAO.getAppointmentList(1, "CONFIRMED")).thenReturn(appointments);
+    assertEquals(2, appointments.size());
+  }
 
 
 }
