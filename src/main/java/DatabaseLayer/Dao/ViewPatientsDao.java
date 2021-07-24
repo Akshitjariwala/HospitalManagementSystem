d0b6538a15ab74ea7ca37839c9d0f5fef1615e26
vjaiswal <vs928999@dal.ca>
2021-07-24 14:51:10 +0530

    Bug fixing

--- src/main/java/DatabaseLayer/Dao/ViewPatientsDao.java
+++ src/main/java/DatabaseLayer/Dao/ViewPatientsDao.java
@@ -13,74 +13,76 @@ import DatabaseLayer.DatabaseConnection.IDatabaseConnectionFactory;
 
 public class ViewPatientsDao {
 
-  Connection connection = null;
-  IDatabaseConnection databaseConnection;
-  IDatabaseConnectionFactory databaseConnectionFactory;
+	Connection connection = null;
+	IDatabaseConnection databaseConnection;
+	IDatabaseConnectionFactory databaseConnectionFactory;
 
-  public ViewPatientsDao() {
-    databaseConnectionFactory = new DatabaseConnectionFactory();
-    databaseConnection = databaseConnectionFactory.getDatabaseConnection();
-  }
+	public ViewPatientsDao() {
+		databaseConnectionFactory = new DatabaseConnectionFactory();
+		databaseConnection = databaseConnectionFactory.getDatabaseConnection();
+	}
 
-  public ArrayList<Patient> getPatients(int docId) {
-    ArrayList<String> patientidlist = getPatientIds(docId);
-    if (patientidlist != null) {
-      ArrayList<Patient> patientlist = new ArrayList<>();
-      connection = databaseConnection.openDBConnection();
-      String query = "Select * from patients where patient_id= ? Limit 1";
-      for (String id : patientidlist) {
-        PreparedStatement statement;
-        try {
-          statement = connection.prepareStatement(query);
-          statement.setString(1, id);
-          ResultSet rs = statement.executeQuery();
-          while (rs.next()) {
-            Patient p = new Patient();
-            p.setUserID(rs.getString("patient_id"));
-            p.setAddress(rs.getString("address"));
-            p.setCityName(rs.getString("city"));
-            p.setEmaiID(rs.getString("email"));
-            p.setEmergencyContactName(rs.getString("emg_contact_name"));
-            p.setEmergencyContactNumber(rs.getString("emg_contact_phone"));
-            p.setFirstName(rs.getString("first_name"));
-            p.setLastName(rs.getString("last_name"));
-            p.setMiddleName(rs.getString("middle_name"));
-            p.setPhoneNumber(rs.getString("phone_number"));
-            p.setStateName(rs.getString("state"));
-            patientlist.add(p);
+	public ArrayList<Patient> getPatients(int docId) {
+		ArrayList<String> patientidlist = getPatientIds(docId);
+		if (patientidlist != null) {
+			ArrayList<Patient> patientlist = new ArrayList<>();
+			connection = databaseConnection.openDBConnection();
+			String query = "Select * from patients where patient_id= ? Limit 1";
 
-          }
-          return patientlist;
+			PreparedStatement statement;
+			try {
+				for (String id : patientidlist) {
+					statement = connection.prepareStatement(query);
+					statement.setString(1, id);
+					ResultSet rs = statement.executeQuery();
+					while (rs.next()) {
+						Patient p = new Patient();
+						p.setUserID(rs.getString("patient_id"));
+						p.setAddress(rs.getString("address"));
+						p.setCityName(rs.getString("city"));
+						p.setEmaiID(rs.getString("email"));
+						p.setEmergencyContactName(rs.getString("emg_contact_name"));
+						p.setEmergencyContactNumber(rs.getString("emg_contact_phone"));
+						p.setFirstName(rs.getString("first_name"));
+						p.setLastName(rs.getString("last_name"));
+						p.setMiddleName(rs.getString("middle_name"));
+						p.setPhoneNumber(rs.getString("phone_number"));
+						p.setStateName(rs.getString("state"));
+						patientlist.add(p);
 
-        } catch (SQLException sqlException) {
-          sqlException.printStackTrace();
-        } finally {
-          databaseConnection.closeDBConnection();
-        }
-      }
-    }
-    return new ArrayList<>();
-  }
+					}
 
-  private ArrayList<String> getPatientIds(int docId) {
-    connection = databaseConnection.openDBConnection();
-    ArrayList<String> patientIdlist = new ArrayList<>();
-    String query = "SELECT * FROM patients_doctors_mapping where doc_id=?";
-    PreparedStatement statement;
-    try {
-      statement = connection.prepareStatement(query);
-      statement.setInt(1, docId);
-      ResultSet rs = statement.executeQuery();
+				} 
+			}catch (SQLException sqlException) {
+					sqlException.printStackTrace();
+				} finally {
+					databaseConnection.closeDBConnection();
+				}
+			return patientlist;
+			}
+		
+		return new ArrayList<>();
+	}
 
-      while (rs.next()) {
-        patientIdlist.add(rs.getString("patient_id"));
-      }
-      return patientIdlist;
-    } catch (SQLException sqlException) {
-      sqlException.printStackTrace();
-    } finally {
-      databaseConnection.closeDBConnection();
-    }
-    return new ArrayList<>();
-  }
+	private ArrayList<String> getPatientIds(int docId) {
+		connection = databaseConnection.openDBConnection();
+		ArrayList<String> patientIdlist = new ArrayList<>();
+		String query = "SELECT * FROM patients_doctors_mapping where doc_id=?";
+		PreparedStatement statement;
+		try {
+			statement = connection.prepareStatement(query);
+			statement.setInt(1, docId);
+			ResultSet rs = statement.executeQuery();
+
+			while (rs.next()) {
+				patientIdlist.add(rs.getString("patient_id"));
+			}
+			return patientIdlist;
+		} catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		} finally {
+			databaseConnection.closeDBConnection();
+		}
+		return new ArrayList<>();
+	}
 }
