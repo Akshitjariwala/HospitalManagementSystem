cc21b6ea1095a5f0ba6a5be0af498f58cd7cce3a
Sai Vaishnavi Jupudi <sv984706@dal.ca>
2021-07-27 18:06:44 +0530

    Added comments to the methods

--- src/main/java/BusinessLogicLayer/AdminModule/ManageReports.java
+++ src/main/java/BusinessLogicLayer/AdminModule/ManageReports.java
@@ -30,6 +30,10 @@ public class ManageReports extends AdminAbstractAction {
     return ACTION_TITLE;
   }
 
+  /* New Reports are added
+   * The report details are taken as input
+   * The details are stored into database
+   */
   public void addReports() {
     Reports report = new Reports();
     Scanner inputData = new Scanner(System.in);
@@ -113,6 +117,11 @@ public class ManageReports extends AdminAbstractAction {
     }
   }
 
+  /* The details of the reports can be updated
+   * An input for the id is taken for which the details are needed to be updated
+   * The details are fetched from database and are displayed
+   * The user will have privilege to edit the details and the details are updated in database
+   */
   public void updateReports() {
 
     Scanner inputData = new Scanner(System.in);
