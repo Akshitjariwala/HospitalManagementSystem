f59b09067482595b892e69c0e223031445effc8d
Sai Vaishnavi Jupudi <sv984706@dal.ca>
2021-07-27 15:49:42 +0530

    Junit to add reports

--- src/test/java/DatabaseLayer/ActionDatabase/Admin/Reports/ManageReportsDAOTest.java
+++ src/test/java/DatabaseLayer/ActionDatabase/Admin/Reports/ManageReportsDAOTest.java
@@ -1,9 +1,13 @@
 package DatabaseLayer.ActionDatabase.Admin.Reports;
 
+import BusinessLogicLayer.BeanClasses.Reports;
 import DatabaseLayer.ActionDatabase.Doctor.ViewReports.ViewReportsDao;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +22,17 @@ class ManageReportsDAOTest {
 
   @Test
   void addReport() {
+    Reports reports = new Reports();
+    reports.setReportId(1);
+    reports.setDoctorId(2);
+    reports.setDate("27-07-2021");
+    reports.setDiagnosisName("Covid-19");
+    reports.setPatientId("vaishnavi");
+    reports.setTestResult("Negative");
+    reports.setTestType("RTPCR");
+    Mockito.when(manageReportsDAO.addReport(reports)).thenReturn(1);
+    assertEquals(1, manageReportsDAO.addReport(reports), "The values are not inserted");
+
   }
 
   @Test
