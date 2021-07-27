113234afe5a8162c665d929aa335cd0805ba1f07
Sai Vaishnavi Jupudi <sv984706@dal.ca>
2021-07-27 15:46:05 +0530

    Mockito tests class for reports

---
+++ src/test/java/DatabaseLayer/ActionDatabase/Admin/Reports/ManageReportsDAOTest.java
@@ -0,0 +1,38 @@
+package DatabaseLayer.ActionDatabase.Admin.Reports;
+
+import DatabaseLayer.ActionDatabase.Doctor.ViewReports.ViewReportsDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManageReportsDAOTest {
+
+  private ManageReportsDAO manageReportsDAO;
+
+  @BeforeEach
+  void setup() {
+    manageReportsDAO = Mockito.mock(ManageReportsDAO.class);
+  }
+
+  @Test
+  void addReport() {
+  }
+
+  @Test
+  void checkDoctorId() {
+  }
+
+  @Test
+  void checkPatientId() {
+  }
+
+  @Test
+  void updateReport() {
+  }
+
+  @Test
+  void getReportsDetails() {
+  }
+}
